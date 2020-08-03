11903d7113057055d316ff2f0f5a45a879503f4d
Nathan Plumb <nplumb@kansys.com>
2020-08-03 10:24:35 -0500

    d

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -3,14 +3,26 @@ import rootReducer from "./reducers/index";
 import reduxImmutableStateInvariant from "redux-immutable-state-invariant";
 //import thunk from "redux-thunk";
 import createSagaMiddleware from "redux-saga";
+import { initSagas } from "../initSagas";
+//import { createLogger } from "redux-logger";
 
 export default function configureStore(initialState) {
     const composeEnhancers =
         window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; // add support for Redux dev tools
+    const sagaMiddleware = createSagaMiddleware();
+    const middleWares = [reduxImmutableStateInvariant(), sagaMiddleware];
+
+    // if (getQuery()["logger"]) {
+    //     middleWares.push(logger);
+    // }
+    const composables = [applyMiddleware(...middleWares)];
+    const enhancer = compose(...composables);
 
     console.info("Saga middleware implemented.");
-    return createStore(
+
+    const store = createStore(
         rootReducer,
+        enhancer,
         initialState,
         composeEnhancers(
             applyMiddleware(
@@ -21,4 +33,7 @@ export default function configureStore(initialState) {
         +window.__REDUX_DEVTOOLS_EXTENSION__ &&
             window.__REDUX_DEVTOOLS_EXTENSION__()
     );
+
+    initSagas(sagaMiddleware);
+    return store;
 }
